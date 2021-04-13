171634f51484b30a48da5265f415c1be09468d34
osakoh <macmive@gmail.com>
2021-04-13 21:43:26 +0100

    added token authentication is settings

--- flightProject/settings.py
+++ flightProject/settings.py
@@ -25,8 +25,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',  # django rest framework
+    'rest_framework.authtoken',  # django rest framework token
     'flightAPP.apps.FlightappConfig'  # flight app
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
