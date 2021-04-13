bdb6f8602bbb50a5b57eaf5f3f6ed62511cc7176
osakoh <macmive@gmail.com>
2021-04-13 17:58:43 +0100

    created serializer for db objects

--- flightAPP/admin.py
+++ flightAPP/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Flight, Passenger, Reservation
 
-# Register your models here.
+
+class PassengerInline(admin.TabularInline):
+    model = Passenger
+
+
+class ReservationInline(admin.TabularInline):
+    model = Reservation
+
+
+@admin.register(Passenger)
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ['firstName', 'lastName', 'middleName', 'email', 'phone']
+    list_display_links = ['firstName', 'lastName', 'middleName', 'email', 'phone']
+    inlines = [ReservationInline]
+
+
+@admin.register(Reservation)
+class Reservation(admin.ModelAdmin):
+    list_display = ['flight', 'passenger']
+    list_display_links = ['flight', 'passenger']
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ['flightNumber', 'operatingAirlines',
+                    'departureCity', 'arrivalCity',
+                    'dateOfDeparture', 'estimatedTimeOfDeparture']
+
+    list_display_links = ['flightNumber', 'operatingAirlines',
+                          'departureCity', 'arrivalCity',
+                          'dateOfDeparture', 'estimatedTimeOfDeparture']
+
+    inlines = [ReservationInline]
--- flightAPP/models.py
+++ flightAPP/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Flight(models.Model):  # parent model of Passenger and Reservaton
+    flightNumber = models.CharField(max_length=15)
+    operatingAirlines = models.CharField(max_length=25)
+    departureCity = models.CharField(max_length=25)
+    arrivalCity = models.CharField(max_length=25)
+    dateOfDeparture = models.DateField()
+    estimatedTimeOfDeparture = models.TimeField()
+
+    def __str__(self):
+        return f"Flight Number:{self.flightNumber}"
+
+
+class Passenger(models.Model):
+    firstName = models.CharField(max_length=25)
+    lastName = models.CharField(max_length=25)
+    middleName = models.CharField(max_length=25)
+    email = models.EmailField(max_length=25)
+    phone = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f"{self.firstName}. {self.middleName[0]} .{self.lastName}"
+
+
+class Reservation(models.Model):
+    flight = models.OneToOneField(Flight, on_delete=models.CASCADE, related_name='flight')
+    passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE, related_name='passenger')
+
+    def __str__(self):
+        return f"Flight Number:{self.flight.flightNumber} - Name: {self.passenger.firstName}.{self.passenger.middleName[0]}.{self.passenger.lastName} "
