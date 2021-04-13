0106ba261adaf8702120ac35e76b0b0e1f90aebf
osakoh <macmive@gmail.com>
2021-04-13 18:03:52 +0100

    added viewset for flight, passenger, and reservation serializers

--- flightAPP/views.py
+++ flightAPP/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Flight, Passenger, Reservation
+from .serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    """
+    viewsets.ReadOnlyModelViewSet: exposes only the read-based operations i.e get(many) & get(single/detail)
+    viewsets.ModelViewSet: handles all pk and non-pk based operations
+    """
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    """
+    viewsets.ReadOnlyModelViewSet: exposes only the read-based operations i.e get(many) & get(single/detail)
+    viewsets.ModelViewSet: handles all pk and non-pk based operations
+    """
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    """
+    viewsets.ReadOnlyModelViewSet: exposes only the read-based operations i.e get(many) & get(single/detail)
+    viewsets.ModelViewSet: handles all pk and non-pk based operations
+    """
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
