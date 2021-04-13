743950462cf894a0edd352c6736a5b3b810051c0
osakoh <macmive@gmail.com>
2021-04-13 20:59:08 +0100

    created custom validation for flight number field

--- flightAPP/serializers.py
+++ flightAPP/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from flightAPP.models import Flight, Passenger, Reservation
+import re
 
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+
+    def validate_flight_number(self, flight_no):
+        """
+        format: validate_fieldName
+        checks if the flight number inputted is alpha-numeric
+        """
+        if re.match("^[a-zA-Z0-9-]*$", flight_no) is None:
+            raise serializers.ValidationError("Invalid Flight Number! Make sure it is alpha-numeric")
+        return flight_no
 
 
 class PassengerSerializer(serializers.ModelSerializer):
