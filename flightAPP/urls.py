7f0bca9d4e0553cadfa3f537c09cb7dcbed77118
osakoh <macmive@gmail.com>
2021-04-14 18:39:23 +0100

    disabled token authentication

--- flightAPP/urls.py
+++ flightAPP/urls.py
@@ -2,15 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
-from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
-
 from . import views
 
 # DefaultRouter object
 router = DefaultRouter()
-# swagger object
-schema_view = get_swagger_view(title='Flight Reservation API')
 
 # register viewsets
 router.register('flights', views.FlightViewSet)
@@ -23,8 +18,5 @@ urlpatterns = [
     path('flight/search/', views.find_flights),
     path('flight/reserve', views.save_reservation),
     # token
-    path('api-token/', obtain_auth_token, name='api_auth_token'),
-    #     documentation
-    path(r'docs/', schema_view),
-
+    # path('api-token/', obtain_auth_token, name='api_auth_token'),
 ]
--- flightProject/urls.py
+++ flightProject/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
+
+# swagger object
+schema_view = get_swagger_view(title='Flight Reservation API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('flightAPP.urls'))
+    path('api/', include('flightAPP.urls')),
+    #     documentation
+    path('documentation/', schema_view),
+    path('docs/', include_docs_urls(title='Flight Reservation API(Core)')),
 ]
