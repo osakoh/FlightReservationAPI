48512baf52368f1a46b1f83b77f820dbd0081fd3
osakoh <macmive@gmail.com>
2021-04-14 14:15:58 +0100

    created testcase for adding a new flight

--- flightAPP/tests.py
+++ flightAPP/tests.py
@@ -5,24 +5,24 @@ from rest_framework.authtoken.models import Token
 from . import views
 
 
+def setup_user():
+    user = get_user_model()
+    return user.objects.create_user(username='test6',
+                                    password='test',
+                                    email='t@t.com')
+
+
 class TestFlight(APITestCase):
     def setUp(self) -> None:
         self.client = APIClient()
-        self.view = views.FlightViewSet.as_view({'get': 'list'})
+        self.view = views.FlightViewSet.as_view({'get': 'list', 'post': 'create'})
         self.uri = '/api/flights/'
-
-    @staticmethod
-    def setup_user():
-        user = get_user_model()
-        return user.objects.create_user(username='test6',
-                                        password='test',
-                                        email='t@t.com')
 
     def test_list(self):
         """
         test the list of flight
         """
-        user = self.setup_user()
+        user = setup_user()
         token = user.auth_token.key
         # adds the appropriate `Authorization:` header on all requests.
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
@@ -30,4 +30,19 @@ class TestFlight(APITestCase):
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'.format(response.status_code))
 
-
+    def test_post_flight(self):
+        user = setup_user()
+        token = user.auth_token.key
+        # adds the appropriate `Authorization:` header on all requests.
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        data = {
+            "flight_number": "ATS-BTN",
+            "operating_airline": "AirTravels",
+            "departure_city": "STN",
+            "arrival_city": "FRT",
+            "date_of_departure": "2021-06-20",
+            "estimated_time_of_departure": "21:30:00"
+        }
+        response = self.client.post(self.uri, data=data)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'.format(response.status_code))
